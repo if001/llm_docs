229fb7667683a37d7e3b8b493ed6afcae10e7e10
windows_on_wsl <otomijuf.004@gmail.com>
2023-06-01 23:57:35 +0900

    fix

--- llm_lora.py
+++ llm_lora.py
@@ -22,13 +22,18 @@ class HuggingFaceLoraPipeline(LLM):
               ) -> str:
         prompt = self._gen_prompt(prompt)
         token_ids = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
-        output_ids = self.model.generate(
-            token_ids,
-            do_sample=True,
-            max_new_tokens=128,
-            temperature=0.7,
-            repetition_penalty=1.1,  
-        )
+        with torch.no_grad():
+            output_ids = self.model.generate(
+                token_ids.to(self.model.device),
+                do_sample=True,
+                max_new_tokens=128,
+                temperature=0.7,
+                repetition_penalty=1.1,
+                pad_token_id=self.tokenizer.pad_token_id,
+                bos_token_id=self.tokenizer.bos_token_id,
+                eos_token_id=self.tokenizer.eos_token_id  
+            )
+        
         output = self.tokenizer.decode(output_ids.tolist()[0][token_ids.size(1):])
         output = output.replace("<NL>", "\n")
         return output
